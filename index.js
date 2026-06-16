60ed7516e9d9b9f8886231b13a602947f107f1eb
Miguel Montero <192806710+miguelmonterodev@users.noreply.github.com>
2026-06-16 12:14:24 +0100

    Create a grid
    Prompt user the size of the square grid
    Test the size of the screen in pixels to calculate with of the boxes

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 const container = document.querySelector('.container')
 
 // Create a box
-function createBox() {
+function createBox(width) {
     const box = document.createElement('div')
     box.classList.add('box')
     container.appendChild(box)
 }
 
+// Prompt the user the size of the grid and create the square grid
+function createGrid() {
+    const gridSize = prompt("What's the size of the grid? Enter a number (example: 16")
+     
+    for(let i=0; i<parseInt(gridSize)**2; i++) {
+        createBox()
+        document.querySelectorAll('.box')[i].style.width = "20px"  // Add width to the boxes
+    }
+    
+}
 // Test
-createBox()
-document.querySelector('.box').style.width = "20px"
+createGrid()
+console.log(typeof(window.innerWidth))
